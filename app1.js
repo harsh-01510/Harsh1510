4bf51f76a6bb2ccbcb4c90e53c44297b0df12c3e
harsh-01510 <sutharharsh1510@gmail.com>
2025-05-13 22:01:58 +0530

    Update app1.js

--- app1.js
+++ app1.js
@@ -2,16 +2,12 @@ document.addEventListener("DOMContentLoaded", function() {
     const form = document.querySelector("form");
     const firstName = document.getElementById("first-name");
     const lastName = document.getElementById("last-name");
-    const email = document.getElementById("email");
-    const mobile = document.getElementById("mobile-number");
     const dob = document.getElementById("dob");
     const address = document.getElementById("address");
     const city = document.getElementById("city");
-    const areapin = document.getElementById("areapin");
     const state = document.getElementById("state");
     const password = document.getElementById("password");
-    const gender = document.querySelectorAll("input[name='gender']");
-
+   
     form.addEventListener("submit", function(event) {
         let valid = true;
 
